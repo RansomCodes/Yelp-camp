951f058e732267511c075e793db971d1c3294ccd
Manmeet <manmeet7210@gmail.com>
2024-08-08 00:05:09 +0530

    Changed database url

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const User=require('./models/user');
 const userRoutes=require('./routes/users'); 
 const { register } = require('module');
 
-const DB_URL='mongodb://localhost:27017/yelp-camp';
+const DB_URL=process.env.DB_URL;
 // mongodb://localhost:27017/yelp-camp
 mongoose.connect(DB_URL);
 
